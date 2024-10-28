b7f10f34631b08afe429705aaa3abb3094755908
goodolcuppa <goodolcuppa@gmail.com>
2024-10-28 15:25:41 +0000

    added config.json configuration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 from curses import wrapper
 import time
+import json
 
 def load_text():
     with open('texts/sample.txt', 'r') as f:
@@ -15,15 +16,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_RED, curses.COLOR_RED)
     INCORRECT_SPACE = curses.color_pair(3)
 
-    v_margin = 2
-    h_margin = 4
-    max_lines = 5
-    input_width = 80
+    with open("config.json") as f:
+        config = json.load(f)
 
     target_text = load_text()
     current_text = []
 
-    stat_height = 1
     words = len(target_text.split(' '))
     words_completed = 0
     accuracy = 1
@@ -31,30 +29,36 @@ def main(stdscr):
     adj_wpm = 0
     start_time = time.time()
 
-    input_v = v_margin + stat_height
-    input_h = h_margin + 1
+    input_top = config["margin_top"] + config["stat_height"]
 
-    input_pad = curses.newpad(100, input_width - 2)
+    input_pad = curses.newpad(100, config["input_width"] - 2)
 
     input_scroll = 0
     input_offset = 2
 
     stdscr.clear()
 
-    stdscr.addstr(input_v, h_margin, '╭' + ('─'*input_width) + '╮')
-    for i in range(max_lines):
-        if i == max_lines // 2:
-            stdscr.addstr(input_v + 1 + i, h_margin, '│>')
-            stdscr.addstr(input_v + 1 + i, h_margin + input_width, '<│')
-        else:
-            stdscr.addstr(input_v + 1 + i, h_margin, '│')
-            stdscr.addstr(input_v + 1 + i, h_margin + input_width + 1, '│')
     stdscr.addstr(
-        input_v + max_lines + 1, h_margin,
-        '╰' + ('─'*input_width) + '╯'
+        input_top, config["margin_left"],
+        '╭' + ('─'*config["input_width"]) + '╮'
+    )
+    for i in range(config["max_lines"]):
+        if i == config["max_lines"] // 2:
+            stdscr.addstr(input_top + 1 + i, config["margin_left"], '│>')
+            stdscr.addstr(
+                input_top + 1 + i, config["margin_left"] + config["input_width"], '<│'
+            )
+        else:
+            stdscr.addstr(input_top + 1 + i, config["margin_left"], '│')
+            stdscr.addstr(
+                input_top + 1 + i, config["margin_left"] + config["input_width"] + 1, '│'
+            )
+    stdscr.addstr(
+        input_top + config["max_lines"] + 1, config["margin_left"],
+        '╰' + ('─'*config["input_width"]) + '╯'
     )
 
-    text_width = input_width - 2
+    text_width = config["input_width"] - 2
     while True:
         input_pad.clear()
         input_pad.addstr(target_text, curses.A_DIM)
@@ -75,22 +79,41 @@ def main(stdscr):
         # display stats
         accuracy = correct_chars / len(current_text) if correct_chars > 0 else 1
         acc_text = f"ACC: {int(accuracy*100)}%"
+        stdscr.addstr(
+            config["margin_top"], config["margin_left"],
+            "{:<20}".format(acc_text)
+        )
+
         time_elapsed = max(time.time() - start_time, 1)
         time_text = f"TIME: {round(time_elapsed)}"
+        stdscr.addstr(
+            config["margin_top"], config["margin_left"] + 60,
+            "{:<20}".format(time_text)
+        )
+
         raw_wpm = (len(current_text) / (time_elapsed / 60)) / 5
         raw_wpm_text = f"WPM (RAW): {round(raw_wpm)}"
+        stdscr.addstr(
+            config["margin_top"], config["margin_left"] + 20,
+            "{:<20}".format(raw_wpm_text)
+        )
+
         adj_wpm = raw_wpm * accuracy
         adj_wpm_text = f"WPM (ADJ): {round(adj_wpm)}"
-        stdscr.addstr(v_margin, h_margin, "{:<20}".format(acc_text))
-        stdscr.addstr(v_margin, h_margin + 20, "{:<20}".format(raw_wpm_text))
-        stdscr.addstr(v_margin, h_margin + 40, "{:<20}".format(adj_wpm_text))
-        stdscr.addstr(v_margin, h_margin + 60, "{:<20}".format(time_text))
+        stdscr.addstr(
+            config["margin_top"], config["margin_left"] + 40,
+            "{:<20}".format(adj_wpm_text)
+        )
+
         stdscr.refresh()
 
         input_pad.refresh(
-            max(0, input_scroll - input_offset), 0, 
-            input_v + 1 + max(0, input_offset - input_scroll), input_h + 1,
-            input_v + max_lines, input_h + input_width - 1
+            max(0, input_scroll - input_offset), 
+            0, 
+            input_top + 1 + max(0, input_offset - input_scroll), 
+            config["margin_left"] + 2,
+            input_top + config["max_lines"], 
+            config["margin_left"] + config["input_width"]
         )
 
         if len(current_text) >= len(target_text):
